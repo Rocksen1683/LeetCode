74a6cb1bf91535e63ed1d037e9cbd357f90e0931
Ekanshh Praveen <edpravee@uwaterloo.ca>
2022-12-08 12:05:38 -0500

    updated nrepeatedelements.cpp

--- C++/n Repeated Elements/nrepeatedElements.cpp
+++ C++/n Repeated Elements/nrepeatedElements.cpp
@@ -1,20 +1,28 @@
-class Solution {
+#include <unordered_map>
+#include <vector>
+
+class Solution
+{
 public:
-    int repeatedNTimes(vector<int>& nums) {
-        int n = nums.size()/2;
+    int repeatedNTimes(vector<int> &nums)
+    {
+        int n = nums.size() / 2;
         std::unordered_map<int, int> numCount;
 
-        for (int i = 0; i<nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++)
+        {
             auto element = numCount.find(nums[i]);
-            if (element != numCount.end()){
-               return nums[i]; 
+            if (element != numCount.end())
+            {
+                return nums[i];
             }
-            else{
-                
-                //not found 
-                numCount[nums[i]] = 1 ;
+            else
+            {
+
+                // not found
+                numCount[nums[i]] = 1;
             }
         }
-        return 0 ;
+        return 0;
     }
 };
