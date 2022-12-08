9cda45ccf5cfd769fa887e13d487e6010dca0a00
Ekanshh Praveen <64133063+Rocksen1683@users.noreply.github.com>
2022-12-07 21:09:25 -0500

    more efficient solution for n-repeated elements

--- nrepeatedElements.py
+++ nrepeatedElements.py
@@ -1,3 +1,18 @@
+""" MORE EFFICIENT VERSION WITHOUT LETTING THE WHOLE DICT FORM""" 
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        n = len(nums)//2
+        numCount = {}
+        for i in nums:
+            if i in numCount.keys():
+                return i
+            else:
+                numCount[i] = 1
+        
+
+
+""" OLD VERSION 
+
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
         n = len(nums)//2
@@ -11,3 +26,4 @@ class Solution:
         for key in numCount.keys():
             if numCount[key] == n:
                 return key
+"""
